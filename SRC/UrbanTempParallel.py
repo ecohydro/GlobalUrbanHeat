f88096496da1cb4d0a616dda293db9128e7862c4
cascadet <cascade.tuholske@gmail.com>
2019-08-26 13:10:53 -0700

    // updates

---
+++ SRC/UrbanTempParallel.py
@@ -0,0 +1,125 @@
+# Program finds the TMax from CHIRTSMax Data and a raster with polygon IDS burned
+# Designed to run through a file list in parallel 
+# By Cascade Tuholske 2019-08-20
+
+# Dependencies
+import rasterio 
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+from rasterstats import zonal_stats
+from rasterio import features
+import os
+import xarray as xr
+import fnmatch
+import time
+import multiprocessing as mp 
+from glob import glob
+from multiprocessing import Pool
+
+
+# LOCAL Test
+DATA_IN = '/Users/cascade/Github/UrbanHeat/data/test_in/' # <<--- path to loop through
+DATA_OUT = '/Users/cascade/Github/UrbanHeat/data/test_out/'
+DATA_INTERIM = '/Users/cascade/Github/UrbanHeat/data/interim/'
+
+
+
+# Loop through dirs in //
+def temp_ghs(dir_nm):
+    
+    # print current process
+    print(mp.current_process())
+
+    # Open the file with GeoPANDAS read_file
+    ghs_ids_fn = 'GHS-UCSB-IDS.csv'
+    ghs_ids_df = pd.read_csv(DATA_INTERIM+ghs_ids_fn)
+
+    # Open Polygon Raster
+    polyRst_fn = 'GHS_UCDB_Raster_touched.tif'
+    polyRst = rasterio.open(DATA_INTERIM+polyRst_fn)
+
+    # Set fn out, change as needed 
+    fn_out = 'GHS-CHIRT-MONTHLY'  
+    
+    # Turn polyRst data as Xarray, 
+    polyRst_da = xr.DataArray(polyRst.read(1), dims = ['y', 'x'])
+    
+    # Start Loop
+    for fn in os.listdir(dir_nm):
+        
+        # Set dir name for writing files
+        dir_year = dir_nm.split(DATA_IN)[1].split('/')[0]
+
+        # find all the tif files
+        if fn.endswith('.tif'):
+
+            # NEED TO BUILD META DATA CHECK INTO ROUTINE and throw an error<<<<---------
+
+            # Get the date of each chirt file
+            date = (fn.split('CHIRTSmax.')[1].split('.tif')[0])
+            print(dir_year)
+            print(date)
+
+            # Open CHIRT Data and turn data into array
+            tempRst = rasterio.open(dir_nm+'/'+fn)
+
+            # Make arrays into x    array DataArray
+            tempRst_da = xr.DataArray(tempRst.read(1), dims = ['y', 'x']) # y and x are our 2-d labels
+
+            # Make xarray dataset
+            ds = xr.Dataset(data_vars = 
+                    {'ghs' : (['y', 'x'], polyRst_da),
+                    'temp' : (['y', 'x'], tempRst_da),})
+
+            # UPDATED 2019-08-19 Mask the CHIRTS PIXELS FIRST, THEN GHS
+            # Mask values from chirt that are ocean in ghs and chirt in our ds 
+            ds_mask = ds.where(ds.temp != -9999, drop = False) #<<<<------ need to double check this
+
+            # Mask pixels for both ghs and chirts where ghs cities are not present
+            ds_mask = ds_mask.where(ds_mask.ghs > 0, drop = False)
+
+            # Group poly_IDs find temp
+            avg = ds_mask.groupby('ghs').mean(xr.ALL_DIMS)
+
+            # turn GHS IDS and avg. CHIRTMax values into 1-D numpy arrays of equal length
+            avg_ID = np.array(avg.ghs)
+            avg_temp = np.array(avg.temp)
+
+            print(len(avg_ID))
+            print(len(avg_temp))
+
+            # turn chirt max and IDS into a DF
+            df_avg = pd.DataFrame()
+            df_avg[date] = avg_temp
+            df_avg['ID_HDC_G0'] = avg_ID
+
+            # merge the df
+            ghs_ids_df = ghs_ids_df.merge(df_avg, on='ID_HDC_G0', how = 'outer') #<<<<----- NEED TO FIX THIS
+
+    ghs_ids_df.to_csv(DATA_OUT+fn_out+'_'+dir_year+'test.csv') # csv out
+    print('DONE ! ! !')
+
+# start pools
+def parallel_loop(function, dir_list, cpu_num):
+    """Run the temp-ghs routine in parallel
+    Args: 
+        function = function to apply in parallel
+        dir_list = list of dir to loop through 
+        cpu_num = numper of cpus to fire  
+    """ 
+    start = time.time()
+    pool = Pool(processes = cpu_num)
+    pool.map(function, dir_list)
+    # pool.map_async(function, dir_list)
+    pool.close()
+
+    end = time.time()
+    print(end-start)
+
+# Get dir list
+dir_list= glob(DATA_IN+'*/')
+dir_list
+
+# Execute code
+parallel_loop(temp_ghs, dir_list, 3)
--- SRC/test.py
+++ SRC/test.py
@@ -20,6 +20,8 @@ CHIRT_DIR = '/Users/cascade/Github/UrbanHeat/data/test_data/' # <<--- path to lo
 #             if fn.endswith('.tif'):
 #                 print(fn)
 
+
+
 for dirpath, dirnames, files in os.walk(CHIRT_DIR):
     print(f'Found directory: {dirpath}')
     print(type(dirpath))
@@ -30,3 +32,20 @@ for dirpath, dirnames, files in os.walk(CHIRT_DIR):
         if fn.endswith('.tif'):
             print(fn)
             print(year)
+
+import multiprocessing as mp
+
+def my_func(x):
+  print(mp.current_process())
+  return x**x
+
+def main():
+  pool = mp.Pool(mp.cpu_count())
+  result = pool.map(my_func, [4,2,3,5,3,2,1,2])
+  result_set_2 = pool.map(my_func, [4,6,5,4,6,3,23,4,6])
+
+  print(result)
+  print(result_set_2)
+
+if __name__ == "__main__":
+  main()
