eb363871d82f635ae47db42cd95092d54d47cfcf
cascadet <cascade.tuholske@gmail.com>
2021-02-15 08:22:58 -0800

    updated

--- src/XX_Add_Region_GPS.py
+++ src/XX_Add_Region_GPS.py
@@ -37,6 +37,9 @@ df_out = ghs[cols]
 df_out.CTR_MN_NM = df_out.CTR_MN_NM.replace('CÃ´te d\'Ivoire', 'Ivory Coast') 
 
 # Get UN regions
+regions_fn = DATA_PATH+'raw/countrylist.csv'
+regions = pd.read_csv(regions_fn)
+
 cols = ['name','region','sub-region','intermediate-region']
 regions = regions[cols]
 regions.rename(columns={'name': 'CTR_MN_NM'}, inplace = True)
@@ -46,4 +49,29 @@ df_out = df_out.merge(regions, on = 'CTR_MN_NM', how = 'inner')
 
 # write out 
 fn_out = DATA_PATH+'interim/GHS-UCDB-IDS.csv'
-out.to_csv(fn_out, index = False)
+df_out.to_csv(fn_out, index = False)
+
+#################################################################################
+#
+#   Countries that have had to be renamed because of discrepancies in formatting
+#   and naming between the ISO and the GHS-UCDB
+#
+#   If you pull ISO country list you may have to update the strings as shown 
+#   below -- CPT 2021.02.15
+#
+#################################################################################
+
+# countries.name = countries.name.replace('Bolivia (Plurinational State of)', 'Bolivia')
+# countries.name = countries.name.replace('Côte d\'Ivoire', 'CÃ´te d\'Ivoire')
+# countries.name = countries.name.replace('Iran (Islamic Republic of)','Iran')
+# countries.name = countries.name.replace('Tanzania, United Republic of','Tanzania')
+# countries.name = countries.name.replace('United States of America','United States')
+# countries.name = countries.name.replace('Venezuela (Bolivarian Republic of)','United States')
+# countries.name = countries.name.replace('Korea (Democratic People\'s Republic of)','North Korea')
+# countries.name = countries.name.replace('Korea, Republic of','South Korea')
+# countries.name = countries.name.replace('Lao People\'s Democratic Republic','Laos')
+# countries.name = countries.name.replace('Syrian Arab Republic','Syria')
+# countries.name = countries.name.replace('Palestine, State of','Palestina')
+# countries.name = countries.name.replace('Congo, Democratic Republic of the','Democratic Republic of the Congo')
+# countries.name = countries.name.replace('Russian Federation','Russia')
+# countries.name = countries.name.replace('Congo','Republic of Congo')
