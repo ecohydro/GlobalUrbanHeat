01b259d90e3cfc869b6e0e2811d78d3c1fd09298
cascadet <cascade.tuholske@gmail.com>
2021-07-21 14:06:07 -0700

    delete old ready to run hi

--- src/02_MakeHeatIndex.py
+++ src/02_MakeHeatIndex.py
@@ -20,7 +20,6 @@ import xarray
 import os
 import glob
 import rasterio
-import matplotlib.pyplot as plt
 import time
 import multiprocessing as mp 
 from multiprocessing import Pool
@@ -28,96 +27,78 @@ import multiprocessing
 import ClimFuncs
 
 #### Functions
-def make_hi(zipped, himax_path_out):
+def hi_loop(year):
     
-    """ Takes an RH and Tmax file zipped together and writes a heat index tif
-    Args:
-        zipped = zipped RH [0] and tmax [1] file path/name
-         himax_path_out = path/to/himax_out 
     """
+    """
+    print(multiprocessing.current_process(), year)
+     
+    # Set up file paths
+    # RH min made with CHIRTS-daily Tmax
+    rh_path = os.path.join('/home/CHIRTS/daily_ERA5/w-ERA5_Td.eq2-2-spechum-Tmax/', str(year)) 
+    # CHIRTS-daily tmax 
+    tmax_path = os.path.join('/home/chc-data-out/products/CHIRTSdaily/v1.0/global_tifs_p05/Tmax/', str(year)) 
+    out_path = os.path.join('/scratch/cascade/UEH-daily/himax/', str(year))
+                            
+    # make dir to write 
+    cmd = 'mkdir '+out_path
+    os.system(cmd)
+    print(cmd)
     
-    # get data date
-    date =zipped[0].split('RH.')[1].split('.tif')[0]
+    # CHIRTS-daily Tmax
+    rh_fns = sorted(glob.glob(rh_path+'/*.tif'))
+    tmax_fns = sorted(glob.glob(tmax_path+'/*.tif'))
+    zipped_list = list(zip(rh_fns,tmax_fns))
     
-    # data type
-    data_out = 'himax'
+    for i, fns in enumerate(zipped_list):
+        # get date
+        date =fns[0].split('RH.')[1].split('.tif')[0]
     
-    # get meta data
-    meta = rasterio.open(zipped[0]).meta
-    meta['dtype'] = 'float64'
+        # data type
+        data_out = 'himax'
     
-    # make hi
-    rh_fn = zipped[0] 
-    tmax_fn = zipped[1]
-    tmax = xarray.open_rasterio(tmax_fn)
-    rh = xarray.open_rasterio(rh_fn)
-    hi = ClimFuncs.heatindex(Tmax = tmax, RH = rh, unit_in = 'C', unit_out = 'C')
+        # get meta data
+        meta = rasterio.open(fns[0]).meta
+        meta['dtype'] = 'float64'
     
-    # get array
-    arr = hi[0]
+        # make hi
+        rh_fn = fns[0] 
+        tmax_fn = fns[1]
+        tmax = xarray.open_rasterio(tmax_fn)
+        rh = xarray.open_rasterio(rh_fn)
+        hi = ClimFuncs.heatindex(Tmax = tmax, RH = rh, unit_in = 'C', unit_out = 'C')
     
-    # write it out
-#     fn_out = os.path.join(himax_path_out,data_out+'.'+date+'.csv')
-#     print(type(arr.data))
-#     np.savetxt(fn_out, arr, delimiter=",")
-    
-    fn_out = os.path.join(himax_path_out,data_out+'.'+date+'.tif')
-    
-    with rasterio.Env():
+        # get array
+        arr = hi.data[0]
+        #print(type(arr))
+        
+        # FN out
+        print(date)
+        fn_out = os.path.join(out_path,data_out+'.'+date+'.tif')
+        print(fn_out)
+        
         with rasterio.open(fn_out, 'w', **meta) as out:
             out.write_band(1, arr)
-
-def hi_loop(zipped_dir):
-    
-    # print process
-    print(multiprocessing.current_process())
-    
-    rh_fns = sorted(glob.glob(zipped_dir[0]+'/*.tif')) # get RH min files
-    tmax_fns = sorted(glob.glob(zipped_dir[1]+'/*.tif')) # get Tmax files
-    zipped_fn_list = list(zip(rh_fns, tmax_fns)) # zipped files names 
-    
-    # get year
-    year = zipped_dir[0].split('Tmax/')[1] 
-
-    # make dir to write files
-    himax_path = os.path.join('/scratch/cascade/UEH-daily/himax/') #/csv # path to write out HImax daily tifs
-    himax_path_out = os.path.join(himax_path, year) 
-    cmd = 'mkdir '+himax_path_out
-    os.system(cmd)
-    print(cmd, 'made')
-    
-    # write himax tifs in a loop
-    test = zipped_fn_list[:4]
-    for zipped in test:#zipped_fn_list:
-        make_hi(zipped, himax_path_out)
-
-def parallel_loop(function, dir_list, cpu_num):
+            
+def parallel_loop(function, start_list, cpu_num):
     """Run a routine in parallel
     Args: 
         function = function to apply in parallel
-        dir_list = list of dir to loop through 
+        start_list = list of args for function to loop through in parallel
         cpu_num = numper of cpus to fire  
     """ 
     start = time.time()
     pool = Pool(processes = cpu_num)
-    pool.map(function, dir_list)
-    # pool.map_async(function, dir_list)
+    pool.map(function, start_list)
     pool.close()
 
     end = time.time()
     print(end-start)
 
-# Set up file paths
-rh_path = os.path.join('/home/CHIRTS/daily_ERA5/w-ERA5_Td.eq2-2-spechum-Tmax/') # RH min made with CHIRTS-daily Tmax
-tmax_path = os.path.join('/home/chc-data-out/products/CHIRTSdaily/v1.0/global_tifs_p05/Tmax/') # CHIRTS-daily Tmax
+if __name__ == "__main__":
+    
+    # Make years 
+    year_list = list(range(1983,2016+1))
 
-# set up list of dirs for parallel loop
-year_list = sorted(os.listdir(rh_path)) # years
-rh_dirs = [os.path.join(rh_path, str(year)) for year in year_list] # rh years dirs
-tmax_dirs = [os.path.join(tmax_path, str(year)) for year in year_list] # rh years dirs
-zipped_dir_list = list(zip(rh_dirs,tmax_dirs)) # zip dirs 
-
-zipped_dir_list = zipped_dir_list[:3]
-
-# Run it
-parallel_loop(function = hi_loop, dir_list = zipped_dir_list, cpu_num = 20)
+    # Run it
+    parallel_loop(function = hi_loop, start_list = year_list, cpu_num = 4)
